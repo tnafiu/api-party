401a1aa4d723fb0bc36ea1237d23c6ee3f538974
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-06-18 15:01:39 -0400

    Add react router and update username in state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch, NavLink } from 'react-router-dom'
+
+import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </div>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/github" component={Github} />
+          <Route
+            render={() => (
+              <p>To get started, click one of the links above.</p>
+            )}
+          />
+        </Switch>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
---
+++ src/Github.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+import './Github.css'
+
+class Github extends Component {
+    state = {
+        username: '',
+      }
+    
+      handleChange = (ev) => {
+        this.setState({ username: ev.target.value })
+      }
+  render() {
+    return (
+      <div className="Github">
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
+
+        <form>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter a GitHub user"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">
+              Look up GitHub user
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Github
